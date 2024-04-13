9fde299bdb74aa861bdd7b2302aa81c50cafe72e
ANDREW KIM <akim227@wisc.edu>
2024-04-12 21:06:05 -0500

    Update 2 files
    - /Frontend.java
    - /FrontendDeveloperTests.java

---
+++ FrontendDeveloperTests.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FrontendDeveloperTests {
+
+    /**
+     * tests frontend functionality
+     */
+    @Test
+    public void test1() {
+
+    }
+
+    /**
+     * tests frontend functionality
+     */
+    @Test
+    public void test2() {
+
+    }
+
+    /**
+     * tests frontend functionality
+     */
+    @Test
+    public void test3() {
+
+    }
+
+    /**
+     * tests frontend functionality
+     */
+    @Test
+    public void test4() {
+
+    }
+}
