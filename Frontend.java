aa981e4efb670c557aa9813c6bc999a22ab32655
ANDREW KIM <akim227@wisc.edu>
2024-04-17 20:36:14 -0500

    Update file Frontend.java

--- Frontend.java
+++ Frontend.java
@@ -3,7 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
