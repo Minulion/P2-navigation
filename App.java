de70b9c7275cda129056a6dc645a72cfa912eace
ANDREW KIM <akim227@wisc.edu>
2024-05-01 22:08:43 -0500

    Update file App.java

--- App.java
+++ App.java
@@ -3,7 +3,10 @@ import javafx.application.Application;
 public class App {
   public static void main(String[] args) {
     System.out.println("v0.1");
-    Frontend.setBackend(new Backend(new DijkstraGraph()));
+    Backend back = new Backend(new DijkstraGraph());
+    back.loadGraphData("campus.dot");
+    Frontend.setBackend(back);
+
     Application.launch(Frontend.class, args);
   }
 }
